a7addaf362f4ce09e94ccf74ecbc4ca5ce30f2bd
gwvsol <gwvsol@gmail.com>
2022-03-24 16:57:41 +0300

    pep8

--- rhvoiceaio/rhvoiceaio/exeption/__init__.py
+++ rhvoiceaio/rhvoiceaio/exeption/__init__.py
@@ -1,6 +1,6 @@
-from rhvoiceaio.exeption.exeption import ConfigError  #, TTSError
+from rhvoiceaio.exeption.exeption import ConfigError  # , TTSError
 
 __all__ = (
     'ConfigError',
-#    'TTSError',
+    # 'TTSError',
 )
